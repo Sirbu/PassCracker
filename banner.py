696a3ff173fd721de008c29cb7bca2da733d1068
Sirbulescu Alexis <alexis.sirbulescu@openmailbox.org>
2016-07-01 17:42:41 +0200

    Passage a python 3

--- banner.py
+++ banner.py
@@ -12,23 +12,23 @@ def getBanner(ip, port):
         socket.setdefaulttimeout(3)     # 3 sec of timeout
         soc = socket.socket()
 
-        print "[+] Connecting to " + str(ip) + " port " + str(port)
+        print("[+] Connecting to " + str(ip) + " port " + str(port))
         soc.connect((ip, port))
-
+        print("COUCOU")
         # here we get the banner of the service runnning
         banner = soc.recv(1024)
-
+        
         # display of the banner and the port associated
-        print "[+] "+ str(port) + " ==> " + banner,
+        print("[+] "+ str(port) + " ==> " + banner)
 
         soc.close()
 
-    except Exception, e:
-        print "[E] " + str(e)
+    except Exception as e:
+        print("[E] " + str(e))
 
 # checking the arguments
 if len(sys.argv) < 2:
-    print "Give at least an IP address you stupid dickhead !"
+    print("Give at least an IP address you stupid dickhead !")
     exit()
 elif len(sys.argv) == 2:
     port_list = [21,22,23,80]
@@ -37,9 +37,9 @@ elif len(sys.argv) == 3:
     port_list.append(sys.argv[2])
 
 ip = sys.argv[1]
-print "IP to check : "+ip
-print "Port(s) to check :",
-print str(port_list).strip('[').strip(']')
+print("IP to check : " + ip)
+print("Port(s) to check :", end="")
+print(str(port_list).strip('[').strip(']'))
 
 for port in port_list:         # lets connect to each port !
     getBanner(ip, port)
--- cuckoo.py
+++ cuckoo.py
@@ -1,20 +1,22 @@
 # python 2.x
+# encoding= utf-8
 
 import os
 import sys
 import crypt
 
+
 def checkFile(filename):
     if not os.path.isfile(filename):
-        print "[-] The file " + filename + " does not exist !"
+        print("[-] The file " + filename + " does not exist !")
         exit(2)
     elif not os.access(filename, os.R_OK):
-        print "[-] Access denied to " + filename
+        print("[-] Access denied to " + filename)
         exit(3)
 
 def main():
     if len(sys.argv) < 3:
-        print "[E] Usage : " + sys.argv[0] + " <password_file.txt>  <dictionnary.txt>"
+        print("[E] Usage : " + sys.argv[0] + " <password_file.txt>  <dictionnary.txt>")
         exit(1)
 
     # First we check the arguments
@@ -23,10 +25,8 @@ def main():
     checkFile(password_file)
     checkFile(dictionnary_file)
 
-    print "[+] Openning file " + password_file + "read-only mode..."
-    fic = open(password_file, 'r')
-
-    print "[+] Some information may not be displayed because of reasons..."
+    print("[+] Openning file " + password_file)
+    fic = open(password_file, 'r', encoding='utf-8')
 
     for line in fic.readlines():
         # the password data contains the id,
@@ -34,9 +34,10 @@ def main():
         user = line.split(':')[0]
         passwd_data = line.split(':')[1]
 
-        # displaying user information
+        # displaying user information for
+        # users that have a password set
         if not (passwd_data == "x" or passwd_data == "!" or passwd_data =="!!"):
-            print "[+] Cracking password for user '" + user +"'"
+            print("[+] Cracking password for user '" + user +"'")
 
             # the variable salt is composed of the
             # id of the algorithm used to hash and
@@ -50,43 +51,46 @@ def main():
 
 def testPass(salt, password, dictionnary_file):
     if len(password) == 0:
-        print "[E] The password is null !"
+        print("[E] The password is null !")
         exit(4)
 
-    dico = open(dictionnary_file, 'r')
+    dico = open(dictionnary_file, 'r', encoding='utf-8')
 
     # for the moment I only do the sha512
     # algorithm. But later, I'll use the hash_type
     # variable to determine which algo must be used
     cpt = 0
-    for word in dico.readlines():
-        # first we must strip out the newline
-        # because it was causing the problem
-        # I've been trying to solve for several hours...
-        # Off course the hash was different that the one
-        # in the password file... BECAUSE THERE IS NO NEWLINE
-        # IN THE PASSWORD FILE !
-        word = word.strip('\n')
+    try:
+        for word in dico.readlines():
+            # first we must strip out the newline
+            # because it was causing the problem
+            # I've been trying to solve for several hours...
+            # Off course the hash was different that the one
+            # in the password file... BECAUSE THERE IS NO NEWLINE
+            # AFTER EACH PASSWORD IN THE PASSWORD FILE !
+            word = word.strip('\n')
 
-        # creating hash of the word
-        # the result will have the follwing
-        # form : $id$salt$Password
-        # so the password must be extracted
-        dico_hash = crypt.crypt(word, '$'+salt[0]+'$'+salt[1]+'$')
+            # creating hash of the word
+            # the result will have the follwing
+            # form : $id$salt$Password
+            # so the password must be extracted
+            dico_hash = crypt.crypt(word, '$'+salt[0]+'$'+salt[1]+'$')
 
-        pass_hash = dico_hash.split('$')[3]
+            pass_hash = dico_hash.split('$')[3]
 
-        if password == pass_hash:
-            print "Password found => " + word
-            return
+            if password == pass_hash:
+                print("Password found => " + word)
+                return 0
 
-        cpt = cpt+1
+            cpt = cpt+1
 
-        print "[+] Word tested : " + str(cpt) + "\r",
-        # print "[DEBUG] Hash generated : " + dico_hash.digest() + " -- " +       "password hash : " + password
+            print("[+] Word tested : " + str(cpt), end='\r')
 
-    print "No password found..."
+    except Exception as e:
+        print("[E] Error : " + str(e))
 
+    print("No password found...")
+    return -1
 
 if __name__ == "__main__":
     main()
